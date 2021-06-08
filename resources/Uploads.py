0df1be7eb52586bedfe383d658e49f5348ac1a69
joe <joe.broder@berkeley.edu>
2021-06-07 19:22:27 -0700

    added with to uploads

--- resources/Uploads.py
+++ resources/Uploads.py
@@ -36,50 +36,46 @@ class UserAvatar(Resource):
     @token_required
     def post(self, user_token, username):
         # get user profile
-        session = DBSession()
-        try:
-            profile=session.query(UserProfile).filter(UserProfile.username == username).one()
-        except:
-            session.close()
-            return {"errors": "User Not Found"}, HTTPStatus.NOT_FOUND
+        with DBSession() as session:
+            try:
+                profile=session.query(UserProfile).filter(UserProfile.username == username).one()
+            except:
+                return {"errors": "User Not Found"}, HTTPStatus.NOT_FOUND
+            
+            # check if post belongs to the authenticated user
+            if profile.username != user_token['username'] and  user_token['privilege'] <= 1:
+                return {"errors": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
+            
         
-        # check if post belongs to the authenticated user
-        if profile.username != user_token['username'] and  user_token['privilege'] <= 1:
-            session.close()
-            return {"errors": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
-        
-      
-        # get file  
-        file = request.files['file'] 
+            # get file  
+            file = request.files['file'] 
 
-        # check if file is of the allowed types
-        if(file.filename.split(".")[1] not in ALLOWED_EXTENSIONS):
-            session.close()
-            return {"errors": "invalid file type"}, HTTPStatus.BAD_REQUEST
-        
-        # create filename
-        bucket_name = "web-template-joebroder"
-        key = ''.join(random.choice(string.ascii_letters) for _ in range(20)) + "." + file.filename.split(".")[1]
+            # check if file is of the allowed types
+            if(file.filename.split(".")[1] not in ALLOWED_EXTENSIONS):
+                return {"errors": "invalid file type"}, HTTPStatus.BAD_REQUEST
+            
+            # create filename
+            bucket_name = "web-template-joebroder"
+            key = ''.join(random.choice(string.ascii_letters) for _ in range(20)) + "." + file.filename.split(".")[1]
 
-        # create aws client
-        s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
-                      aws_secret_access_key=SECRET_KEY)
+            # create aws client
+            s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY,
+                        aws_secret_access_key=SECRET_KEY)
 
-        # upload file
-        s3.upload_fileobj(file, BUCKET_NAME, key)
+            # upload file
+            s3.upload_fileobj(file, BUCKET_NAME, key)
 
-        location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
-        url = "https://%s.s3-%s.amazonaws.com/%s" % (bucket_name, location, key)
+            location = s3.get_bucket_location(Bucket=bucket_name)['LocationConstraint']
+            url = "https://%s.s3-%s.amazonaws.com/%s" % (bucket_name, location, key)
 
-        # delete old pfp
-        if(profile.avatar!= None):
-            s3.delete_object(Bucket = BUCKET_NAME,Key=profile.avatar.split("/")[-1])
+            # delete old pfp
+            if(profile.avatar!= None):
+                s3.delete_object(Bucket = BUCKET_NAME,Key=profile.avatar.split("/")[-1])
 
-        # set pfp
-        profile.avatar = url
-        session.commit()
-        session.close()
+            # set pfp
+            profile.avatar = url
+            session.commit()
 
-        return HTTPStatus.OK
+            return HTTPStatus.OK
 
 
